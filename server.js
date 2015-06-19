d527c300995c7f52dc9ebb94a359e989e507f34f
andrewheartsxd <andrewheartsxd@users.noreply.github.com>
2015-06-18 22:56:19 -0700

    clean up console logs, added documentation comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-// server.js
+
 var WebSocketServer = require('ws').Server;  
 var port = process.env.PORT || 7000;
 var mongoose = require('mongoose');
@@ -11,22 +11,22 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/overload_chat'
 var wss = new WebSocketServer({port: port});
 console.log('websocket server created, listening on port: ' + port);
 
+// broadcasts messages to all connected users
 wss.broadcast = function(data) {
   for (var i in this.clients)
     this.clients[i].send(data);
 };
 
+// client connect
 wss.on("connection", function(ws) {
   console.log("websocket connection open");
 
+  // looks for and displays cached messages in db 
   MessageStore.findOne({})
   .populate('messageStore')
   .exec(function(err, msgs) {
     if (err) throw err;
-    console.log("msgs: ");
-    console.log(msgs);
     if (!msgs) {
-      console.log("no messages in messageStore");
       return;
     } 
     for (var i = 0; i < msgs.messageStore.length; i++) {
@@ -34,27 +34,22 @@ wss.on("connection", function(ws) {
     }
   }); 
   
+  // parses message sent by client 
   ws.on("message", function(message) {
     var client = {};
     var parsedMessage = JSON.parse(message);
-    console.log("parsedMessage: ");
-    console.dir(parsedMessage);
     ws.nickName = parsedMessage.nickName;
     ws.msgBody = parsedMessage.msgBody;
 
     var newMessage = new Message();
     newMessage.nickName = ws.nickName;
     newMessage.msgBody = ws.msgBody;
-
-    console.log("nickName: " + ws.nickName);
-    console.log("messageBody: " + ws.msgBody);
     
+    // broadcasts message, saves to db
     if (ws.msgBody) {
       wss.broadcast(ws.nickName + ": " + ws.msgBody);
       newMessage.save(function(err, message) {
         if (err) throw err;
-        //console.log(message.nickName);
-        //console.log(message.msgBody);
         MessageStore.findOne({}, function(err, foundMsgStore) {
           if (err) throw err;
 
@@ -66,37 +61,21 @@ wss.on("connection", function(ws) {
               console.log("result: ", result);
             });
           } else {
-            console.log("foundMsgStore " + foundMsgStore);
+            // only keeps 10 messages in cache
             if (foundMsgStore.messageStore.length > 10) foundMsgStore.messageStore.shift();
             foundMsgStore.messageStore.push(message._id);
             foundMsgStore.save(function(err, savedMessageStore) {
               if (err) throw err;
-              console.log("savedMessageStore: " + savedMessageStore);
             });
           }
         }); 
       });
     }
-    //else {
-    //  // New user -> Show all past messages
-    //  MessageStore.findOne({})
-    //  .populate('messageStore')
-    //  .exec(function(err, msgs) {
-    //    if (err) throw err;
-    //    console.log("msgs: ");
-    //    console.log(msgs);
-    //    if (!msgs) {
-    //      console.log("no messages in messageStore");
-    //      return;
-    //    } 
-    //    for (var i = 0; i < msgs.messageStore.length; i++) {
-    //      ws.send(msgs.messageStore[i].nickName + ": " + msgs.messageStore[i].msgBody); 
-    //    }
-    //  }); 
-    //}
   }); 
-
+  
+  // client disconnect
   ws.on("close", function() {
     console.log("websocket connection closed");
   });
+
 });
